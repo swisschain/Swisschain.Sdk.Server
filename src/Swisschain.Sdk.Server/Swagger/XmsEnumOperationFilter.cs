dd291d769a0c6a28da6ec4a3bdcaf0b9fe0607ae
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-02-21 01:28:15 +0300

    ConfigureSwaggerGenOptions method added XmsEnum added for swagger

---
+++ src/Swisschain.Sdk.Server/Swagger/XmsEnumOperationFilter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Swisschain.Sdk.Server.Swagger
+{
+    internal class XmsEnumOperationFilter : IOperationFilter
+    {
+        private readonly XmsEnumExtensionsOptions _options;
+
+        public XmsEnumOperationFilter(XmsEnumExtensionsOptions options)
+        {
+            _options = options;
+        }
+
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            var invalidParameters = context.ApiDescription.ParameterDescriptions.Where(p => p.Type == null).ToArray();
+
+            if (invalidParameters.Any())
+            {
+                var invalidParameter = invalidParameters.First();
+                var message =
+                    $@"Invalid parameter found. Probably parameter source mismatched. (e.g. You specified [FromQuery] for the path parameter).
+
+Action: {context.ApiDescription?.ActionDescriptor.DisplayName}
+Relative path: {context.ApiDescription?.RelativePath}
+OperationId: {operation.OperationId}
+Parameter: {invalidParameter?.Name}
+Expected parameter source: {invalidParameter?.Source.DisplayName}";
+                throw new InvalidOperationException(message);
+            }
+            
+            foreach (var parameter in GetEnumParameters(context))
+            {
+                var operationParameter = operation.Parameters.Single(p => p.Name == parameter.Name);
+
+                XmsEnumExtensionApplicator.Apply(operationParameter.Extensions, parameter.Type, _options);
+            }
+        }
+
+        private static IEnumerable<(Type Type, string Name)> GetEnumParameters(OperationFilterContext context)
+        {
+            return context
+                .ApiDescription
+                .ParameterDescriptions
+                .Select(p => (Type: TryGetEnumType(p), Name: p.Name))
+                .Where(x => x.Type != null);
+        }
+
+        private static Type TryGetEnumType(ApiParameterDescription parameter)
+        {
+            if (parameter.Type.GetTypeInfo().IsEnum)
+            {
+                return parameter.Type;
+            }
+
+            var nullableUnderlyingType = Nullable.GetUnderlyingType(parameter.Type);
+
+            return nullableUnderlyingType?.GetTypeInfo().IsEnum == true
+                ? nullableUnderlyingType 
+                : null;
+        }
+    }
+}
---
+++ src/Swisschain.Sdk.Server/Swagger/XmsEnumSchemaFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Swisschain.Sdk.Server.Swagger
+{
+    internal class XmsEnumSchemaFilter : ISchemaFilter
+    {
+        private readonly XmsEnumExtensionsOptions _options;
+
+        public XmsEnumSchemaFilter(XmsEnumExtensionsOptions options)
+        {
+            _options = options;
+        }
+
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            var type = context.Type;
+            if (type.IsEnum)
+            {
+                XmsEnumExtensionApplicator.Apply(schema.Extensions, type, _options);
+            };
+        }
+    }
+}
