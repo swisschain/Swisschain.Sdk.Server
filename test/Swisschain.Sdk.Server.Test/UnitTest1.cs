36b2c78476862ee02627da333bb74261ad9d5cc3
Aleksandr Belousov <alexandr.belousov@lykke.com>
2020-02-20 14:39:29 +0400

    Added symbols in nuget pack step

---
+++ test/Swisschain.Sdk.Server.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Swisschain.Sdk.Server.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
