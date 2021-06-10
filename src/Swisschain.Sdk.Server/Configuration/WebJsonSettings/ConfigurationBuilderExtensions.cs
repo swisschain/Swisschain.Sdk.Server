2faecda73df37fbe4c1e822655ce28f809bb910f
Andrey Volkov <avolkov.dev.10x@gmail.com>
2021-06-10 16:59:11 +0300

    SIR-2289 Add hack to be able to read http configuration multiple times

--- src/Swisschain.Sdk.Server/Configuration/WebJsonSettings/ConfigurationBuilderExtensions.cs
+++ src/Swisschain.Sdk.Server/Configuration/WebJsonSettings/ConfigurationBuilderExtensions.cs
@@ -1,5 +1,10 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
 using Serilog;
 
 namespace Swisschain.Sdk.Server.Configuration.WebJsonSettings
@@ -13,9 +18,9 @@ namespace Swisschain.Sdk.Server.Configuration.WebJsonSettings
         {
             try
             {
-                var stream = client.GetStreamAsync(url).ConfigureAwait(false).GetAwaiter().GetResult();
+                var jsonData = client.GetStringAsync(url).ConfigureAwait(false).GetAwaiter().GetResult();
 
-                builder.AddJsonStream(stream);
+                builder.AddJsonFile(new InMemoryFileProvider(jsonData), "nonsense.json", false, false);
             }
             catch (HttpRequestException ex) when (isOptional)
             {
@@ -23,6 +28,30 @@ namespace Swisschain.Sdk.Server.Configuration.WebJsonSettings
             }
 
             return builder;
+        }
+        
+        // we can't use AddJsonStream because we have to call configurationBuilder.Build() multiple times to substitute secrets
+        // it's not possible until https://github.com/dotnet/runtime/issues/43788 is fixed
+        internal class InMemoryFileProvider : IFileProvider
+        {
+            private class InMemoryFile : IFileInfo
+            {
+                private readonly byte[] data;
+                public InMemoryFile(string json) => data = Encoding.UTF8.GetBytes(json);
+                public Stream CreateReadStream() => new MemoryStream(data);
+                public bool Exists => true;
+                public long Length => data.Length;
+                public string PhysicalPath => null;
+                public string Name => null;
+                public DateTimeOffset LastModified { get; } = DateTimeOffset.UtcNow;
+                public bool IsDirectory => false;
+            }
+
+            private readonly IFileInfo fileInfo;
+            public InMemoryFileProvider(string json) => this.fileInfo = new InMemoryFile(json);
+            public IFileInfo GetFileInfo(string _) => this.fileInfo;
+            public IDirectoryContents GetDirectoryContents(string _) => null;
+            public IChangeToken Watch(string _) => NullChangeToken.Singleton;
         }
     }
 }
